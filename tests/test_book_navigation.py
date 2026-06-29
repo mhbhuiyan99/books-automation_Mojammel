2ec7f90bf7aa16ffce5e3422516377ae8b541304
Mojammel Haque Bhuiyan <mhbhuiyan10023@gmail.com>
2026-06-29 15:29:58 +0600

    feat: verify selected books open the correct details page, verify that book informatin displayed on the homepage

---
+++ tests/test_book_navigation.py
@@ -0,0 +1,17 @@
+from pages.home_page import HomePage
+
+def test_random_book_navigation(page):
+    
+    home = HomePage(page)
+
+    home.Open()
+    indices = home.random_book_indices()
+
+    for index in indices:
+        expected_title = home.get_book_title(index)  # Capture the book title
+
+        home.open_book(index)  # Click the book
+        actual_title = home.get_detail_title()
+        assert expected_title == actual_title  # H1 title matches selected book title
+
+        home.go_back()  # Navigate back to homepage
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,21 +1,23 @@
-def test_homepage(page):
-    page.goto("https://books.toscrape.com/index.html")
+from pages.home_page import HomePage
 
-    print("Current URL: ",page.url)
-    print("Page Title: ", page.title())
-    assert page.url == "https://books.toscrape.com/index.html"
+def test_homepage(page):
+
+    home = HomePage(page)
+    home.open()
+
+    assert page.url == HomePage.URL
     assert page.title() == "All products | Books to Scrape - Sandbox"
 
-    headings =  page.locator("h1, h2, h3, h4, h5, h6")
-    print(headings)
-    print(headings.count())
+    headings = page.locator("h1,h2,h3,h4,h5,h6")
+
+    assert headings.count() > 0
 
     for i in range(headings.count()):
+
         heading = headings.nth(i)
         assert heading.is_visible()
 
         text = heading.text_content() or ""
         assert text.strip() != ""
 
-        print(text)
-
+    assert home.get_books_count() > 0
