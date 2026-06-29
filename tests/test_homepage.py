9172f48472c21c7ee9e0acb75f2c5e534482fe39
Mojammel Haque Bhuiyan <mhbhuiyan10023@gmail.com>
2026-06-29 13:19:50 +0600

    feat: heading is visible, heading text is not empty

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -5,3 +5,17 @@ def test_homepage(page):
     print("Page Title: ", page.title())
     assert page.url == "https://books.toscrape.com/index.html"
     assert page.title() == "All products | Books to Scrape - Sandbox"
+
+    headings =  page.locator("h1, h2, h3, h4, h5, h6")
+    print(headings)
+    print(headings.count())
+
+    for i in range(headings.count()):
+        heading = headings.nth(i)
+        assert heading.is_visible()
+
+        text = heading.text_content() or ""
+        assert text.strip() != ""
+
+        print(text)
+
